9b3eb0bcd3cf9c55c760bd0e686c699ac05fa439
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-25 09:21:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers,message){
+    return drivers.filter(function(driver){
+        return driver===message || driver===message.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(function(retutnValue){
+        return retutnValue===string || retutnValue===string['0']
+    })
+}
+
+function matchName(drivers,string){
+    return drivers.filter(function (newValue){
+        return newValue['name']===string
+    })
+}
